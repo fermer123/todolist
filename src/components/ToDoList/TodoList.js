2a3db4c5c7a171e9810f3be8b1b3020eefa90e2f
fermer123 <fermer_525@mail.ru>
2022-01-06 03:54:38 +0500

    37

--- src/components/ToDoList/TodoList.js
+++ src/components/ToDoList/TodoList.js
@@ -1,26 +1,43 @@
+import { useState } from 'react';
+
 export const TodoList = (props) => {
+  const [edit, setEdit] = useState(null);
+
   const deleteTodo = (id) => {
-    let newTodo = [...props.todo].filter((item) => item.id != id);
+    let newTodo = [...props.todo].filter((item) => item.id !== id);
     props.setTodo(newTodo);
   };
 
   let statusTodo = (id) => {
     let newTodo = [...props.todo].filter((item) => {
-      if (item.id == id) {
+      if (item.id === id) {
         item.status = !item.status;
       }
       return item; //возращает оставшиеся элементы массива
     });
     props.setTodo(newTodo);
   };
-  console.log(props);
+
+  const editTodo = (id) => {
+    setEdit(id);
+  };
+
   return (
     <div>
       {props.todo.map((item) => (
         <div key={item.id}>
+          {edit === item.id ? (
+            <div>
+              <input />
+              <button>Save</button>
+            </div>
+          ) : (
+            <div>{item.title}</div>
+          )}
           <div>{item.title}</div>
           <button onClick={() => deleteTodo(item.id)}> Delete</button>
           <button onClick={() => statusTodo(item.id)}> Close/Open</button>
+          <button onClick={() => editTodo(item.id)}> Edit</button>
         </div>
       ))}
     </div>
--- src/components/addToDo/AddTodo.js
+++ src/components/addToDo/AddTodo.js
@@ -2,20 +2,22 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 export const AddTodo = (props) => {
-  console.log(props);
   const [value, setValue] = useState('');
 
   const saveTodo = () => {
-    return props.setTodo([
-      ...props.todo,
-      {
-        id: uuidv4(),
-        title: value,
-        status: true,
-      },
-    ]);
+    return props.setTodo(
+      [
+        ...props.todo,
+        {
+          id: uuidv4(),
+          title: value,
+          status: true,
+        },
+      ],
+      setValue(''),
+    );
   };
-  console.log(props);
+
   return (
     <div>
       <input
